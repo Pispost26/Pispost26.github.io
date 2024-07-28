8688e4eb44be2d475e08190f2e4c98de83e082d0
Pispost26 <127264366+Pispost26@users.noreply.github.com>
2024-07-28 14:40:37 -0500

    Cambio resolucion

--- script.js
+++ script.js
@@ -36,3 +36,18 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
+function openModal(img) {
+    var modal = document.getElementById("myModal");
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+    modal.style.display = "block";
+    modalImg.src = img.src;
+    captionText.innerHTML = img.alt;
+
+    var span = document.getElementsByClassName("close")[0];
+    span.onclick = function() { 
+        modal.style.display = "none";
+    }
+}
+
